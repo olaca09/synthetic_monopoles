Numba working with incorrect result

scripts/magfield.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy.constants import mu_0 as mu
 from scipy.constants import pi as pi
 from matplotlib import pyplot as plt
+import numba
+from numba import njit
 
 def simplewire(nr, lablength, I, overwrite=False):
     ##Returns a placeholder field corresponding to a current I through a wire along the
@@ -154,6 +156,7 @@ def oppositecoils(nr, lablength, I, overwrite=False):
         np.save(f'saves/fields/oppositecoils{I}field{nr},{lablength}.npy', field)
         return field
 
+@njit(cache=True, parallel=True, fastmath=True)
 def oppositecoilspos(field, pos):
     ##Returns the field at position pos corresponding to two currents I of opposing directions through square coils 
     ##placed orthogonally to the z-axis centred 1/3th from the edges of the lab. Takes nr as the number of points to consider along each wire.
@@ -175,85 +178,70 @@ def oppositecoilspos(field, pos):
     wire7 = np.array([lablength+qpos,lablength+qpos]) #Current in positive x far from z=0 and y=0 (pos in y,z)
     wire8 = np.array([lablength+qpos,-qpos]) #Current in positive y far from z=0 and close to x=0 (pos in z,x)
 
-    #Generate field at point
+    #Generate field at pos
     #Integrate the field per Biot-Savart along all currents
-    Bx, By, Bz = 0, 0, 0
+    B = np.array((0,0,0), dtype=numba.float64)
     #Currents along x:
     for px in range(br):
         dx = [stepr, 0, 0]
-        #wire1
-        distance = [pos[0]-(px*stepr-qpos), pos[1]-wire1[0], pos[2]-wire1[1]]
-        dBx1, dBy1, dBz1 = (mu*I/(4*pi) * np.cross(dx, distance, axisb=0,
-                            axisc=0)/(np.linalg.norm(distance)**3))
-        Bx += dBx1
-        By += dBy1
-        Bz += dBz1
-    
-        #wire3
-        distance = [pos[0]-(px*stepr-qpos), pos[1]-wire3[0], pos[2]-wire3[1]]
-        dBx3, dBy3, dBz3 = -(mu*I/(4*pi) * np.cross(dx, distance, axisb=0,
-                            axisc=0)/(np.linalg.norm(distance)**3))
-        Bx += dBx3
-        By += dBy3
-        Bz += dBz3
+        # wire1
+        distance = np.array((pos[0]-(px*stepr-qpos), pos[1]-wire1[0],
+                            pos[2]-wire1[1]))
+        dB = (mu*I/(4*pi) * np.cross(dx, distance)/(np.linalg.norm(distance)**3))
+        B += dB
+
+        # wire3
+        distance = np.array((pos[0]-(px*stepr-qpos), pos[1]-wire3[0],
+                             pos[2]-wire3[1]))
+        dB = (mu*I/(4*pi) * np.cross(dx, distance)/(np.linalg.norm(distance)**3))
+        B += dB
 
         #wire5
-        distance = [pos[0]-(px*stepr-qpos), pos[1]-wire5[0], pos[2]-wire5[1]]
-        dBx5, dBy5, dBz5 = -(mu*I/(4*pi) * np.cross(dx, distance, axisb=0,
-                            axisc=0)/(np.linalg.norm(distance)**3))
-        Bx += dBx5
-        By += dBy5
-        Bz += dBz5
+        distance = np.array((pos[0]-(px*stepr-qpos), pos[1]-wire5[0],
+                             pos[2]-wire5[1]))
+        dB = (mu*I/(4*pi) * np.cross(dx, distance)/(np.linalg.norm(distance)**3))
+        B += dB
 
         #wire7
-        distance = [pos[0]-(px*stepr-qpos), pos[1]-wire7[0], pos[2]-wire7[1]]
-        dBx7, dBy7, dBz7 = (mu*I/(4*pi) * np.cross(dx, distance, axisb=0,
-                            axisc=0)/(np.linalg.norm(distance)**3))
-        Bx += dBx7
-        By += dBy7
-        Bz += dBz7
+        distance = np.array((pos[0]-(px*stepr-qpos), pos[1]-wire7[0],
+                             pos[2]-wire7[1]))
+        dB = (mu*I/(4*pi) * np.cross(dx, distance)/(np.linalg.norm(distance)**3))
+        B += dB
 
 
     #Currents along y:
     for py in range(br):
         dy = [0, stepr, 0]
         #wire2
-        distance = [pos[0]-wire2[1], pos[1]-(py*stepr-qpos), pos[2]-wire2[0]]
-        dBx2, dBy2, dBz2 = (mu*I/(4*pi) * np.cross(dy, distance, axisb=0,
-                            axisc=0)/(np.linalg.norm(distance)**3))
-        Bx += dBx2
-        By += dBy2
-        Bz += dBz2
-    
-        #wire4
-        distance = [pos[0]-wire4[1], pos[1]-(py*stepr-qpos), pos[2]-wire4[0]]
-        dBx4, dBy4, dBz4 = -(mu*I/(4*pi) * np.cross(dy, distance, axisb=0,
-                            axisc=0)/(np.linalg.norm(distance)**3))
-        Bx += dBx4
-        By += dBy4
-        Bz += dBz4
-    
-        #wire6
-        distance = [pos[0]-wire6[1], pos[1]-(py*stepr-qpos), pos[2]-wire6[0]]
-        dBx6, dBy6, dBz6 = -(mu*I/(4*pi) * np.cross(dy, distance, axisb=0,
-                            axisc=0)/(np.linalg.norm(distance)**3))
-        Bx += dBx6
-        By += dBy6
-        Bz += dBz6
-    
-        #wire8
-        distance = [pos[0]-wire8[1], pos[1]-(py*stepr-qpos), pos[2]-wire8[0]]
-        dBx8, dBy8, dBz8 = (mu*I/(4*pi) * np.cross(dy, distance, axisb=0,
-                            axisc=0)/(np.linalg.norm(distance)**3))
-        Bx += dBx8
-        By += dBy8
-        Bz += dBz8
+        distance = np.array((pos[0]-wire2[1], pos[1]-(py*stepr-qpos),
+                             pos[2]-wire2[0]))
+        dB = (mu*I/(4*pi) * np.cross(dx, distance)/(np.linalg.norm(distance)**3))
+        B += dB
 
-    Br, Bth, Bph = cart_to_sph(np.array([Bx,By,Bz])) #Express as spherical coordinates
-    retfield = np.array((Bx, By, Bz, Br, Bth, Bph))
+        #wire4
+        distance = np.array((pos[0]-wire4[1], pos[1]-(py*stepr-qpos),
+                             pos[2]-wire4[0]))
+        dB = (mu*I/(4*pi) * np.cross(dx, distance)/(np.linalg.norm(distance)**3))
+        B += dB
+
+        #wire6
+        distance = np.array((pos[0]-wire6[1], pos[1]-(py*stepr-qpos),
+                             pos[2]-wire6[0]))
+        dB = (mu*I/(4*pi) * np.cross(dx, distance)/(np.linalg.norm(distance)**3))
+        B += dB
+
+        #wire8
+        distance = np.array((pos[0]-wire8[1], pos[1]-(py*stepr-qpos),
+                             pos[2]-wire8[0]))
+        dB = (mu*I/(4*pi) * np.cross(dx, distance)/(np.linalg.norm(distance)**3))
+        B += dB
+
+    Bsph = cart_to_sph(B) #Express as spherical coordinates
+    retfield = np.append(B, Bsph)
 
     return retfield
 
+@njit(cache=True, parallel=True, fastmath=True)
 def griddot(a, b):
     ##Returns the dot product for each point in the supplied grids a, b. Contracts the
     ##first dimension.
@@ -264,6 +252,7 @@ def griddot(a, b):
     #result = np.where(result == 0, 1e-20, result)
     return result
 
+@njit(cache=True, parallel=True, fastmath=True)
 def cart_to_sph(cart):
     ##Returns spherical coordinates of the form(r, polar, azimuthal) for the given cartesian 
     ##coordinates of the form (x,y,z) takes an array with coords as the first dimension.

scripts/synfieldsolver.py
@@ -1,38 +1,84 @@
 import numpy as np
-import magfield as mg
 import synfieldtools as sn
 import pickle
-from numpy import pi as pi
+import time
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
 
 def main():
-    #Set field and modes
-    I = 10 #Current parameter for the field
-    br = 10 #Number of points to consider along each wire when integrating external field
-    norot = False #Whether to ignore rotational degrees of freedom
-    nosyn = 'False' #Whether to ignore synthetic fields, accepts the strings 'False',
-                    #'True', 'Nomag' and 'Noscalar'
-    overwriteresult = False #Whether to overwrite previous ODE results
-    alternatestreams = True #Whether to use alternate swarming scheme
-    
-    #Define parameters
+    # Parse arguments
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-r', '--norot', action='store_true',
+                        help='Ignores rotation if set')
+    parser.add_argument('-s', '--nosyn', default='False', type=str,
+                        help='Ignores synthetic fields if set to True')
+    parser.add_argument('-o', '--overwrite', action='store_true',
+                        help='Always generate new data if set')
+    parser.add_argument('-a', '--alternatestreams', action='store_false',
+                        help='Overrides "nosyn", integrates a'
+                        + ' single starting point for all combinations of'
+                        + ' fields. Is on by default, turns off if set.')
+    parser.add_argument('-c', '--current', default=10, type=float,
+                        help='Current in amperes flowing through coils')
+    parser.add_argument('--br', default=10, type=int, help='Integration'
+                        + ' granularity along coil side')
+    parser.add_argument('--nr', default=1e5, type=int, help='Differentiation'
+                        + ' step is lab length divided by this number')
+    parser.add_argument('--lablength', default=1e-3, type=float, help='Size'
+                        + ' of lab cube side in meters')
+    parser.add_argument('--tmax', default=0.5, type=float, help='Maximum'
+                        + ' flight time in seconds')
+    parser.add_argument('-j', '--jcouple', default=1e5, type=float,
+                        help='Spin-spin coupling strength')
+    parser.add_argument('-g', '--gammacouple', default=1e8, type=float,
+                        help='Spin-field coupling strength')
+    parser.add_argument('-m', '--mass', default=3.58e-27, type=float,
+                        help='The total mass of dumbbell in kg')
+    parser.add_argument('-l', '--dumbbellength', default=5e-5, type=float,
+                        help='The distance between dumbbell edges in m')
+    args = vars(parser.parse_args())
 
-    nr = 1e5 #Number of points to divide the lab length by for numerical differentiation
-             #step size
-    lablength = 1e-3 #Cube side of lab in m
-    tmax = 0.5 #Trajectory time in s
-    J = 1e5 #Spin-spin coupling strength
-    Gamma = 1e8 #Spin-field coupling strength
-    
-    mass0 = 3.58e-27 #The total mass of the dumbbell in kg, as a placeholder this is the mass of
-                     #two silver atoms
-    len0 = 5e-5 #The distance between dumbbell edges in m
-    mass = np.repeat(mass0, 5) #Full mass vector
+    # Set field and modes
+    current = args['current']  # Current parameter for the field
+    # Number of points to consider along each wire when integrating external
+    # field
+    br = args['br']
+    norot = args['norot']  # Whether to ignore rotational degrees of freedom
+    # Whether to ignore synthetic fields, accepts the strings 'False',
+    # 'True', 'Nomag' and 'Noscalar'
+    nosyn = args['nosyn']  # Must be a string.
+    # Whether to overwrite previous ODE results
+    overwriteresult = args['overwrite']
+    # Whether to use alternate swarming scheme
+    alternatestreams = args['alternatestreams']
+
+    # Define parameters
+
+    # Number of points to divide the lab length by for numerical
+    # differentiation step size
+    nr = args['nr']
+    # Cube side of lab in m
+    lablength = args['lablength']
+    # Trajectory time in s
+    tmax = args['tmax']
+    # Spin-spin coupling strength
+    J = args['jcouple']
+    # Spin-field coupling strength
+    Gamma = args['gammacouple']
+    # The total mass of the dumbbell in kg, as a placeholder this is the mass
+    # of two silver atoms
+    mass0 = args['mass']
+    # The distance between dumbbell edges in m
+    len0 = args['dumbbellength']
+    # Full mass vector
+    mass = np.repeat(mass0, 5)
     mass[3] = mass0*len0**2/4
     mass[4] = mass[3]
 
-    field = (br, lablength, I) #Tuple collecting field parameters
+    # Tuple collecting field parameters
+    field = (br, lablength, current)
 
-    #Set parameters
+    # Set parameters
     sn.nr = nr
     sn.lablength = lablength
     sn.tmax = tmax
@@ -43,98 +89,138 @@ def main():
     sn.mass = mass
     sn.field = field
 
-    #Set initial position, velocity and the fast eigenstate to consider
-    step = lablength/nr
+    # Set initial position, velocity and the fast eigenstate to consider
+    # step = lablength/nr
     initposarray = np.array((0, 0, 0, 0, 0))
-    swarmnum = 4 #Square root of number of streams in swarm
-    swarmgrid = np.mgrid[0:lablength:swarmnum*1j,0:lablength:swarmnum*1j] #Grid to swarm the initial positions
+    # Square root of number of streams in swarm
+    swarmnum = 4
+    # Grid to swarm the initial positions
+    swarmgrid = np.mgrid[0:lablength:swarmnum*1j, 0:lablength:swarmnum*1j]
     swarmgrid = np.insert(swarmgrid, 2, np.zeros(swarmgrid.shape[1:3]), axis=0)
     swarmgrid = np.insert(swarmgrid, 2, np.zeros(swarmgrid.shape[1:3]), axis=0)
     swarmgrid = np.insert(swarmgrid, 0, np.zeros(swarmgrid.shape[1:3]), axis=0)
-    initposarray = initposarray[:,None,None] + swarmgrid
-    altinitpos = initposarray[:,1,1] #Starting position for alternate swarming method
+    initposarray = initposarray[:, None, None] + swarmgrid
+    # Starting position for alternate swarming method
+    altinitpos = initposarray[:, 1, 1]
     initvel = (1e-2, 0, 0, 0, 0)
     eigenstate = 2
 
-    try: #Try to load pregenerated result
-        with open(f'saves/odesols/result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}vel{initvel}swarmnum{swarmnum}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin', 'rb') as file:
+    # Try to load pregenerated result
+    try:
+        with open(f'''saves/odesols/result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}vel{initvel}swarmnum{swarmnum}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin''', 'rb') as file:
             sol = pickle.load(file)
-            print(f'Loading result from file result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}vel{initvel}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin')
+            print(f'''Loaded result from file result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}vel{initvel}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin''')
     except FileNotFoundError:
         overwriteresult = True
-        
-    if overwriteresult: #Only perform calculations if result not avaiable on save:
-    
-        #Integrate paths
+
+    # Only perform calculations if result not avaiable on save:
+    if overwriteresult:
+        # Integrate paths
         print('Integrating dynamics . . . Please wait')
         sol = []
         if alternatestreams:
-            print(f'Simulating start {tuple(altinitpos)} with synthetic fields')
+            print(f'Simulating start {tuple(altinitpos)} with synthetic'
+                  + ' fields')
+            start = time.time()
             try:
-                stream = sn.solvedyn(tuple(altinitpos[:]), initvel, eigenstate, norot, 'False')
+                stream = sn.solvedyn(tuple(altinitpos[:]), initvel, eigenstate,
+                                     norot, 'False')
                 stream.color = 'red'
                 sol.append(stream)
                 print(f'Stream number {len(sol)} has been integrated!')
             except Exception as e:
                 print(e)
-                print(f'A stream has failed to generate, most probably due to crossing the centre')
-            print(f'Simulating start {tuple(altinitpos)} without synthetic fields')
+                print('A stream has failed to generate, most probably due to'
+                      + ' crossing the centre')
+            end = time.time()
+            print(f'This took {end - start} seconds')
+
+            print(f'Simulating start {tuple(altinitpos)} without synthetic'
+                  + ' fields')
+            start = time.time()
             try:
-                stream = sn.solvedyn(tuple(altinitpos[:]), initvel, eigenstate, norot, 'True')
+                stream = sn.solvedyn(tuple(altinitpos[:]), initvel, eigenstate,
+                                     norot, 'True')
                 stream.color = 'blue'
                 sol.append(stream)
                 print(f'Stream number {len(sol)} has been integrated!')
             except Exception as e:
                 print(e)
-                print(f'A stream has failed to generate, most probably due to crossing the centre')
-            print(f'Simulating start {tuple(altinitpos)} without the magnetic synthetic field')
+                print('A stream has failed to generate, most probably due to'
+                      + ' crossing the centre')
+            end = time.time()
+            print(f'This took {end - start} seconds')
+
+            print(f'Simulating start {tuple(altinitpos)} without the magnetic'
+                  + ' synthetic field')
+            start = time.time()
             try:
-                stream = sn.solvedyn(tuple(altinitpos[:]), initvel, eigenstate, norot, 'Nomag')
+                stream = sn.solvedyn(tuple(altinitpos[:]), initvel, eigenstate,
+                                     norot, 'Nomag')
                 stream.color = 'green'
                 sol.append(stream)
                 print(f'Stream number {len(sol)} has been integrated!')
             except Exception as e:
                 print(e)
-                print(f'A stream has failed to generate, most probably due to crossing the centre')
-            print(f'Simulating start {tuple(altinitpos)} without the scalar synthetic field')
+                print('A stream has failed to generate, most probably due to'
+                      + ' crossing the centre')
+            end = time.time()
+            print(f'This took {end - start} seconds')
+
+            print(f'Simulating start {tuple(altinitpos)} without the scalar' 
+                  + ' synthetic field')
+            start = time.time()
             try:
-                stream = sn.solvedyn(tuple(altinitpos[:]), initvel, eigenstate, norot, 'Noscalar')
+                stream = sn.solvedyn(tuple(altinitpos[:]), initvel, eigenstate,
+                                     norot, 'Noscalar')
                 stream.color = 'orange'
                 sol.append(stream)
                 print(f'Stream number {len(sol)} has been integrated!')
             except Exception as e:
                 print(e)
-                print(f'A stream has failed to generate, most probably due to crossing the centre')
+                print('A stream has failed to generate, most probably due to'
+                      + ' crossing the centre')
+            end = time.time()
+            print(f'This took {end - start} seconds')
 
         else:
             for i in range(initposarray.shape[1]):
                 for j in range(initposarray.shape[2]):
-                    print(tuple(initposarray[:,i,j]))
+                    print(tuple(initposarray[:, i, j]))
+                    start = time()
                     try:
-                        sol.append(sn.solvedyn(tuple(initposarray[:,i,j]), initvel, eigenstate, norot, nosyn))
+                        sol.append(sn.solvedyn(
+                            tuple(initposarray[:, i, j]), initvel, eigenstate,
+                            norot, nosyn))
                         print(f'Stream number {len(sol)} has been integrated!')
                     except Exception as e:
                         raise e
                         print(e)
-                        print(f'A stream has failed to generate, most probably due to crossing the centre')
-        
-        #Save the result
-        print(f'Saving result to file resultfield{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}vel{initvel}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin')
-        with open(f'saves/odesols/result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}vel{initvel}swarmnum{swarmnum}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin', 'wb') as file:
+                        print('A stream has failed to generate, most probably'
+                              + ' due to crossing the centre')
+                    end = time.time()
+                    print(f'This took {end - start} seconds')
+
+        # Save the result
+        print(f'''Saving result to file resultfield{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}vel{initvel}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin''')
+        with open(f'''saves/odesols/result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}vel{initvel}swarmnum{swarmnum}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin''', 'wb') as file:
             pickle.dump(sol, file)
 
     else:
         print('Loading previously generated result')
-    #Extract positions and orientations of the some stream
-    pos = sol[0].y[0:3,:]
-    vel = sol[0].y[5:8,:]
-    ori = sol[0].y[3:5,:]
-    #Print the result
+    # Extract positions and orientations of the some stream
+    # pos = sol[0].y[0:3, :]
+    # vel = sol[0].y[5:8, :]
+    ori = sol[0].y[3:5, :]
+    # Print the result
     print('Rotation integrated:')
     print(ori)
-    print(pos)
-    
-    sn.lineplot(sol, initvel, swarmnum, eigenstate, norot, nosyn, alternatestreams)
+    # print('Position integrated:')
+    # print(pos)
+
+    sn.lineplot(sol, initvel, swarmnum, eigenstate, norot, nosyn,
+                alternatestreams)
+
 
 if __name__ == '__main__':
     main()