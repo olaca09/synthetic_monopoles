bde8a6de4b163ee9453dcfaad2d4a73a019f3b9c
Ola Carlsson <ola.carlsson@perumal.se>
2023-10-10 21:19:31 +0200

    Working looper for square coils

---
+++ scripts/synfieldloop.py
@@ -0,0 +1,96 @@
+import synfieldsolver as sfs
+import multiprocessing
+import numpy as np
+from itertools import product
+
+
+# Iterates sunfieldsolver through ranges of parameters
+def main():
+    norot = True
+    nosyn = 'False'
+
+    difference = False
+    overwriteresult = False
+    br = 10
+    nr = 1e5
+    lablength = 1e-3
+    tmax = 0.5
+    noplot = True
+    quiver = False
+    alternatestreams = True
+    parameter = False
+    # Ranges of looped par. of the form (start, stop, step)
+    currentr = (10, 10, 1)
+    jr = (0, 0, 1)
+    gammar = (1e8, 1e8, 1)  # Standard 1e8
+    massr = (3.58e-27, 3.58e-27, 1)  # Standard 3.58e-27
+    dlr = (5e-5, 5e-5, 1)  # dumbellength
+    vxr = (2e-3, 3e-3, 4)
+    vyr = (0, 0, 1)
+    vzr = (0, 0, 1)
+    vthetar = (0, 0, 1)
+    vphir = (0, 0, 1)
+    xr = (0, 0, 1)
+    yr = (1/6, 1/2, 3)
+    zr = (1/6, 1/2, 3)
+    thetar = (0, 0, 1)
+    phir = (0, 0, 1)
+
+    for current, J, Gamma, mass0, len0 in product(
+                                          np.linspace(currentr[0], currentr[1],
+                                                      currentr[2]),
+                                          np.linspace(jr[0], jr[1], jr[2]),
+                                          np.linspace(gammar[0], gammar[1],
+                                                      gammar[2]),
+                                          np.linspace(massr[0], massr[1],
+                                                      massr[2]),
+                                          np.linspace(dlr[0], dlr[1], dlr[2])):
+        for velx, vely, velz, velth, velphi in product(np.linspace(vxr[0],
+                                                                   vxr[1],
+                                                                   vxr[2]),
+                                                       np.linspace(vyr[0],
+                                                                   vyr[1],
+                                                                   vyr[2]),
+                                                       np.linspace(vzr[0],
+                                                                   vzr[1],
+                                                                   vzr[2]),
+                                                       np.linspace(vthetar[0],
+                                                                   vthetar[1],
+                                                                   vthetar[2]),
+                                                       np.linspace(vphir[0],
+                                                                   vphir[1],
+                                                                   vphir[2])):
+            for x, y, z, th, phi in product(np.linspace(xr[0], xr[1], xr[2]),
+                                            np.linspace(yr[0], yr[1], yr[2]),
+                                            np.linspace(zr[0], zr[1], zr[2]),
+                                            np.linspace(thetar[0], thetar[1],
+                                                        thetar[2]),
+                                            np.linspace(phir[0], phir[1],
+                                                        phir[2])):
+                initpos = np.array((x, y, z, th, phi))
+                initvel = (velx, vely, velz, velth, velphi)
+                p = multiprocessing.Process(target=sfs.main, name="sfs",
+                                            args=(current, br, norot, nosyn,
+                                                  difference, noplot,
+                                                  overwriteresult,
+                                                  alternatestreams, quiver,
+                                                  parameter, nr, lablength,
+                                                  tmax, initvel,
+                                                  initpos, J, Gamma,
+                                                  mass0, len0))
+                p.start()
+
+                # Terminate process after timeout seconds
+                timeout = 9000
+                p.join(timeout)
+                if p.is_alive():
+                    print('Synfieldsolver '
+                          + f'current{current}vel{initvel}pos{initpos}J{J}Gamma{Gamma}mass{mass0}len0{len0}'
+                          + ' took more than {timeout}'
+                          + 'seconds to finish, skipping parameter selection')
+                    p.terminate()
+                    p.join()
+
+
+if __name__ == '__main__':
+    main()
--- scripts/synfieldsolver.py
+++ scripts/synfieldsolver.py
@@ -5,93 +5,10 @@ import time
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
-def main():
-    # Parse arguments
-    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-r', '--norot', action='store_true',
-                        help='Ignores rotation if set')
-    parser.add_argument('-d', '--difference', action='store_true',
-                        help='Displays difference between synthetic '
-                        + 'streams and nosyn if set')
-    parser.add_argument('-s', '--nosyn', default='False', type=str,
-                        help='Ignores synthetic fields if set to True')
-    parser.add_argument('-o', '--overwrite', action='store_true',
-                        help='Always generate new data if set')
-    parser.add_argument('-a', '--alternatestreams', action='store_false',
-                        help='Overrides "nosyn", integrates a'
-                        + ' single starting point for all combinations of'
-                        + ' fields. Is on by default, turns off if set.')
-    parser.add_argument('-q', '--quiver', action='store_true',
-                        help='Plot external field if set')
-    parser.add_argument('-p', '--parameter', action='store_true',
-                        help='Plots streams in parameter space if true,'
-                        + ' currently way too slow')
-    parser.add_argument('-c', '--current', default=10, type=float,
-                        help='Current in amperes flowing through coils')
-    parser.add_argument('--br', default=10, type=int, help='Integration'
-                        + ' granularity along coil side')
-    parser.add_argument('--nr', default=1e5, type=int, help='Differentiation'
-                        + ' step is lab length divided by this number')
-    parser.add_argument('--lablength', default=1e-3, type=float, help='Size'
-                        + ' of lab cube side in meters')
-    parser.add_argument('--tmax', default=0.5, type=float, help='Maximumy'
-                        + ' flight time in seconds')
-    parser.add_argument('-v', '--velocity', nargs='+', default=(1e-2, 0, 0, 0, 0),
-                        type=float, help='Initial velocity in m/s')
-    parser.add_argument('-j', '--jcouple', default=1e5, type=float,
-                        help='Spin-spin coupling strength')
-    parser.add_argument('-g', '--gammacouple', default=1e8, type=float,
-                        help='Spin-field coupling strength')
-    parser.add_argument('-m', '--mass', default=3.58e-27, type=float,
-                        help='The total mass of dumbbell in kg')
-    parser.add_argument('-l', '--dumbbellength', default=5e-5, type=float,
-                        help='The distance between dumbbell edges in m')
-    parser.add_argument('-np', '--noplot', action='store_true',
-                        help='Does not show plot if set')
-    args = vars(parser.parse_args())
+def main(current, br, norot, nosyn, difference, noplot, overwriteresult,
+         alternatestreams, quiver, parameter, nr, lablength, tmax, initvel,
+         initpos, J, Gamma, mass0, len0):
 
-    # Set field and modes
-    current = args['current']  # Current parameter for the field
-    # Number of points to consider along each wire when integrating external
-    # field
-    br = args['br']
-    norot = args['norot']  # Whether to ignore rotational degrees of freedom
-    # Whether to ignore synthetic fields, accepts the strings 'False',
-    # 'True', 'Nomag' and 'Noscalar'
-    nosyn = args['nosyn']  # Must be a string.
-    # Whether to plot differences to nosyn instead of regular streams
-    difference = args['difference']
-    # Whether to hide plot
-    noplot = args['noplot']
-    # Whether to overwrite previous ODE results
-    overwriteresult = args['overwrite']
-    # Whether to use alternate swarming scheme
-    alternatestreams = args['alternatestreams']
-    # Whether to plot external field
-    quiver = args['quiver']
-    # Whether to plot in parameter space
-    parameter = args['parameter']
-
-    # Define parameters
-
-    # Number of points to divide the lab length by for numerical
-    # differentiation step size
-    nr = args['nr']
-    # Cube side of lab in m
-    lablength = args['lablength']
-    # Trajectory time in s
-    tmax = args['tmax']
-    # Initial velocity in m/s
-    initvel = tuple(args['velocity'])
-    # Spin-spin coupling strength
-    J = args['jcouple']
-    # Spin-field coupling strength
-    Gamma = args['gammacouple']
-    # The total mass of the dumbbell in kg, as a placeholder this is the mass
-    # of two silver atoms
-    mass0 = args['mass']
-    # The distance between dumbbell edges in m
-    len0 = args['dumbbellength']
     # Full mass vector
     mass = np.repeat(mass0, 5)
     mass[3] = mass0*len0**2/4
@@ -123,19 +40,21 @@ def main():
     swarmgrid = np.insert(swarmgrid, 0, np.zeros(swarmgrid.shape[1:3]), axis=0)
     initposarray = initposarray[:, None, None] + swarmgrid
     # Starting position for alternate swarming method
-    altinitpos = initposarray[:, 1, 1]
+    altinitpos = initpos * (lablength, lablength, lablength, np.pi, 2*np.pi)
     eigenstate = 2
 
     # Try to load pregenerated result
+    print(f"Trying to load from file saves/odesols/result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}pos({initpos[0]:.2f}, {initpos[1]:.2f}, {initpos[2]:.2f}, {initpos[3]:.2f}, {initpos[4]:.2f})vel{initvel}swarmnum{swarmnum}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin")
     try:
-        with open(f'''saves/odesols/result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}vel{initvel}swarmnum{swarmnum}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin''', 'rb') as file:
+        with open(f'''saves/odesols/result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}pos({initpos[0]:.2f}, {initpos[1]:.2f}, {initpos[2]:.2f}, {initpos[3]:.2f}, {initpos[4]:.2f})vel{initvel}swarmnum{swarmnum}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin''', 'rb') as file:
             sol = pickle.load(file)
-            print(f'''Loaded result from file result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}vel{initvel}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin''')
+            print(f'''Loaded result from file result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}pos({initpos[0]:.2f}, {initpos[1]:.2f}, {initpos[2]:.2f}, {initpos[3]:.2f}, {initpos[4]:.2f})vel{initvel}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin''')
     except FileNotFoundError:
         overwriteresult = True
 
     # Only perform calculations if result not avaiable on save:
     if overwriteresult:
+        print(f"pos({initpos[0]:.2f}, {initpos[1]:.2f}, {initpos[2]:.2f}, {initpos[3]:.2f}, {initpos[4]:.2f})")
         # Integrate paths
         print('Integrating dynamics . . . Please wait')
         sol = []
@@ -188,7 +107,7 @@ def main():
             end = time.time()
             print(f'This took {end - start} seconds')
 
-            print(f'Simulating start {tuple(altinitpos)} without the scalar' 
+            print(f'Simulating start {tuple(altinitpos)} without the scalar'
                   + ' synthetic field')
             start = time.time()
             try:
@@ -208,7 +127,7 @@ def main():
             for i in range(initposarray.shape[1]):
                 for j in range(initposarray.shape[2]):
                     print(tuple(initposarray[:, i, j]))
-                    start = time()
+                    start = time.time()
                     try:
                         sol.append(sn.solvedyn(
                             tuple(initposarray[:, i, j]), initvel, eigenstate,
@@ -223,8 +142,8 @@ def main():
                     print(f'This took {end - start} seconds')
 
         # Save the result
-        print(f'''Saving result to file resultfield{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}vel{initvel}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin''')
-        with open(f'''saves/odesols/result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}vel{initvel}swarmnum{swarmnum}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin''', 'wb') as file:
+        print(f'''Saving result to file resultfield{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}pos({initpos[0]:.2f}, {initpos[1]:.2f}, {initpos[2]:.2f}, {initpos[3]:.2f}, {initpos[4]:.2f})vel{initvel}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin''')
+        with open(f'''saves/odesols/result_field{field}nr{nr}tmax{tmax}J{J}Gamma{Gamma}mass{mass0}len{len0}n{eigenstate}pos({initpos[0]:.2f}, {initpos[1]:.2f}, {initpos[2]:.2f}, {initpos[3]:.2f}, {initpos[4]:.2f})vel{initvel}swarmnum{swarmnum}norot{norot}nosyn{nosyn}altstream{alternatestreams}.bin''', 'wb') as file:
             pickle.dump(sol, file)
 
     else:
@@ -249,9 +168,105 @@ def main():
         for stream in sol:
             stream.y = stream.y[:, :max_step] - refstream.y[:, :max_step]
 
-    sn.lineplot(sol, initvel, swarmnum, eigenstate, norot, nosyn,
+    sn.lineplot(sol, initpos, initvel, swarmnum, eigenstate, norot, nosyn,
                 difference, alternatestreams, quiver, parameter, noplot)
 
 
 if __name__ == '__main__':
-    main()
+    # Parse arguments
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-r', '--norot', action='store_true',
+                        help='Ignores rotation if set')
+    parser.add_argument('-d', '--difference', action='store_true',
+                        help='Displays difference between synthetic '
+                        + 'streams and nosyn if set')
+    parser.add_argument('-s', '--nosyn', default='False', type=str,
+                        help='Ignores synthetic fields if set to True')
+    parser.add_argument('-o', '--overwrite', action='store_true',
+                        help='Always generate new data if set')
+    parser.add_argument('-a', '--alternatestreams', action='store_false',
+                        help='Overrides "nosyn", integrates a'
+                        + ' single starting point for all combinations of'
+                        + ' fields. Is on by default, turns off if set.')
+    parser.add_argument('-q', '--quiver', action='store_true',
+                        help='Plot external field if set')
+    parser.add_argument('-par', '--parameter', action='store_true',
+                        help='Plots streams in parameter space if true,'
+                        + ' currently way too slow')
+    parser.add_argument('-c', '--current', default=10.0, type=float,
+                        help='Current in amperes flowing through coils')
+    parser.add_argument('--br', default=10, type=int, help='Integration'
+                        + ' granularity along coil side')
+    parser.add_argument('--nr', default=1e5, type=int, help='Differentiation'
+                        + ' step is lab length divided by this number')
+    parser.add_argument('--lablength', default=1e-3, type=float, help='Size'
+                        + ' of lab cube side in meters')
+    parser.add_argument('--tmax', default=0.5, type=float, help='Maximum'
+                        + ' flight time in seconds')
+    parser.add_argument('-v', '--velocity', nargs='+', default=(1e-2, 0, 0, 0,
+                                                                0),
+                        type=float, help='Initial velocity in m/s')
+    parser.add_argument('-p', '--position', nargs='+', default=(0, 1/3, 1/3, 0,
+                                                                0),
+                        type=float, help='Initial position as fractions of lab'
+                        + ' length and full rotation')
+    parser.add_argument('-j', '--jcouple', default=1e5, type=float,
+                        help='Spin-spin coupling strength')
+    parser.add_argument('-g', '--gammacouple', default=1e8, type=float,
+                        help='Spin-field coupling strength')
+    parser.add_argument('-m', '--mass', default=3.58e-27, type=float,
+                        help='The total mass of dumbbell in kg')
+    parser.add_argument('-l', '--dumbbellength', default=5e-5, type=float,
+                        help='The distance between dumbbell edges in m')
+    parser.add_argument('-np', '--noplot', action='store_true',
+                        help='Does not show plot if set')
+    args = vars(parser.parse_args())
+
+    # Set field and modes
+    current = args['current']  # Current parameter for the field
+    # Number of points to consider along each wire when integrating external
+    # field
+    br = args['br']
+    norot = args['norot']  # Whether to ignore rotational degrees of freedom
+    # Whether to ignore synthetic fields, accepts the strings 'False',
+    # 'True', 'Nomag' and 'Noscalar'
+    nosyn = args['nosyn']  # Must be a string.
+    # Whether to plot differences to nosyn instead of regular streams
+    difference = args['difference']
+    # Whether to hide plot
+    noplot = args['noplot']
+    # Whether to overwrite previous ODE results
+    overwriteresult = args['overwrite']
+    # Whether to use alternate swarming scheme
+    alternatestreams = args['alternatestreams']
+    # Whether to plot external field
+    quiver = args['quiver']
+    # Whether to plot in parameter space
+    parameter = args['parameter']
+
+    # Define parameters
+
+    # Number of points to divide the lab length by for numerical
+    # differentiation step size
+    nr = args['nr']
+    # Cube side of lab in m
+    lablength = args['lablength']
+    # Trajectory time in s
+    tmax = args['tmax']
+    # Initial velocity in m/s
+    initvel = tuple(args['velocity'])
+    # Initial position as fractions of lab length and full rotation
+    initpos = np.array((args['position']))
+    # Spin-spin coupling strength
+    J = args['jcouple']
+    # Spin-field coupling strength
+    Gamma = args['gammacouple']
+    # The total mass of the dumbbell in kg, as a placeholder this is the mass
+    # of two silver atoms
+    mass0 = args['mass']
+    # The distance between dumbbell edges in m
+    len0 = args['dumbbellength']
+
+    main(current, br, norot, nosyn, difference, noplot, overwriteresult,
+         alternatestreams, quiver, parameter, nr, lablength, tmax, initvel,
+         initpos, J, Gamma, mass0, len0)
